8f3d564559ef04b4e73cbc9f19a15dfd6fea515e
Subanista <subanista14@gmail.com>
2023-12-06 11:12:06 +0530

    modified

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose= require('mongoose')
+const productRoute=require('./routes/productRoute')
+const userRoute=require('./routes/userRoute')
+const errorMiddleware=require('./middleware/errorMiddleware')
+
 const app = express()
 
+//const MONGO_URI=process.env.MONGO_URI
+//const PORT=process.env.PORT||3000;
 
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/products',productRoute);
+//app.use('/api/users',userRoute);
 //routes
+app.get('/blog',(req,res)=>{
+    res.send('Hello blog my name is suba')
+})
+
 app.get('/',(req,res)=>{
-    res.send('Hello Node API')
+    
+   res.send('Hello Node API')
 })
-app.listen(3000,()=>{
-    console.log('Node API app is running on port 3000')
-})
+
+
+app.use(errorMiddleware);
+
+mongoose.
+connect('mongodb+srv://admin:admin@nodeapi.btcywoe.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(()=>{
+    console.log('connected to mongodb')
+    app.listen(3000,()=>{
+        console.log('Node API app is running on port 3000')
+    })
+}).catch((error=>{
+    console.log('error')
+}))
